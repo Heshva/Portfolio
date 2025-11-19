6ce830f1c3fd2496b60b8beede1e90a2aee278ff
heshvaa <heshvasoni1006@gmail.com>
2025-11-19 22:51:17 +0530

    new fix

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -13,11 +13,11 @@ export default function WorkExperience({}: Props) {
                  px-10 max-w-full mx-auto overflow-hidden"
     >
       <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl mt-20 mb-10">
-        Experience :
+        Experience 
       </h3>
 
       <div
-        className="w-full flex space-x-10 overflow-x-scroll p-10 
+        className="w-full flex justify-center items-center space-x-10 overflow-x-scroll p-10 
                    snap-x snap-mandatory overflow-hidden"
       >
         <ExprerienceCard />
